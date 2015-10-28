4a6ea09a45c77b689c675c1fb2d2e86f624cb80b
zhangb <xiaobo1128@gmail.com>
2015-10-28 17:32:29 +0800

    update counter

--- counter.go
+++ counter.go
@@ -1,16 +1,42 @@
 package main
 
+import (
+	"log"
+)
+
 type Counter struct {
-	ch chan uint64 //
+	next int64
+	ch   chan int64
 }
 
-func RunCounter(next uint64, counter chan Counter) {
-	count := next
-	for {
-		select {
-		case c := <-counter:
-			count++
-			c.ch <- count
+func NewCounter(next_id int64) *Counter {
+	return &Counter{
+		next: next_id,
+		ch:   make(chan int64),
+	}
+}
+
+func (c Counter) Run() {
+	go func() {
+		defer func() {
+			if r := recover(); r != nil {
+				log.Printf("Counter run loop is terminating.  Detail: %v\n", r)
+			}
+		}()
+		for {
+			if c.ch != nil {
+				c.ch <- c.next
+				c.next++
+			} else {
+				log.Print("Counter is closed.")
+			}
 		}
+	}()
+}
+
+func (c Counter) Close() {
+	if c.ch != nil {
+		close(c.ch)
+		c.ch = nil
 	}
 }
