1eaa56440c4438535b9cc21bd873860e3d8b8d42
zhangb <xiaobo1128@gmail.com>
2015-10-29 11:21:53 +0800

    add md5 short url

--- encoder.go
+++ encoder.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"crypto/md5"
+	"encoding/hex"
+	"io"
+	"log"
 	"math"
+	"strconv"
 	"strings"
 )
 
@@ -28,4 +33,46 @@ func decodeURL(s string) int {
 	}
 
 	return num
+}
+
+func md5ShortUrl1(url string) string {
+	h := md5.New()
+	io.WriteString(h, url)
+	hx := h.Sum(nil)
+	var result [4]string
+	s := hex.EncodeToString(hx)
+	length := len(s) / 8
+	for i := 0; i < length; i++ {
+		subhex := s[i*8 : i*8+8]
+		bit, _ := strconv.ParseInt(subhex, 16, 64)
+		var chHex = 0x3FFFFFFF & bit
+		var outchars = ""
+		for i := 0; i < 6; i++ {
+			//base 62 3D base32 1F
+			//比base值少2
+			val := 0x0000001F & chHex
+			outchars += ALPHABET[val : val+1]
+			chHex = chHex >> 5
+		}
+		result[i] = outchars
+	}
+	log.Println(result)
+
+	return result[0]
+}
+
+//idx range (0-3)
+func md5ShortUrl2(url string, idx int) string {
+	md5Hex := md5.Sum([]byte(url))
+	md5Hash := hex.EncodeToString(md5Hex[:])
+	shortStr := ""
+	subStr := md5Hash[idx*8 : (idx+1)*8]
+	x, _ := strconv.ParseInt(subStr, 16, 0)
+	x = x & 0x3fffffff
+	for k := 0; k < 6; k++ {
+		index := 0x0000003d & x
+		shortStr += ALPHABET[index : index+1]
+		x = x >> 5
+	}
+	return shortStr
 }
