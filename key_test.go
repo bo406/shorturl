2b4a35aa256afdc4167b3874b87b03952559dc2a
zhangb <xiaobo1128@gmail.com>
2015-10-29 15:31:42 +0800

    update test.

--- key_test.go
+++ key_test.go
@@ -3,26 +3,23 @@ package main
 import "testing"
 
 func TestGen(t *testing.T) {
-	counter := NewCounter(int64(100000))
-	counter.Run()
-	n := <-counter.ch
+	n := int64(100000)
 	s := GenKey(n)
 	if s != "0aU" {
 		t.Fatalf("gen key error, n: %d, %s, %s", n, s, "0aU")
 	}
 	id := GenId(s)
-	if id != 100000 {
-		t.Fatalf("gen id error, %d, %d", id, 100000)
+	if id != n {
+		t.Fatalf("gen id error, %d, %d", id, n)
 	}
 }
 
 func BenchmarkGen(b *testing.B) {
 	b.StartTimer()
-	counter := NewCounter(int64(100000))
-	counter.Run()
+	n := int64(100000)
 	for i := 0; i < b.N; i++ {
-		n := <-counter.ch
 		s := GenKey(n)
 		GenId(s)
+		n++
 	}
 }
