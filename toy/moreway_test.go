93fb9e01a73e88d4db2207ac3ed4a3abd9db108e
zhangb <xiaobo1128@gmail.com>
2015-10-29 17:39:03 +0800

    增加玩具, 使用其他算法生成短域名

--- toy/moreway_test.go
+++ toy/moreway_test.go
@@ -3,9 +3,10 @@ package main
 import "testing"
 
 func TestEncoder(t *testing.T) {
-	counter := NewCounter(int64(100000))
-	counter.Run()
-	n := <-counter.ch
+	//	counter := NewCounter(int64(100000))
+	//	counter.Run()
+	//	n := <-counter.ch
+	n := int64(100000)
 	s := generateURL(n)
 	if s != "0aU" {
 		t.Fatalf("gen key error, id: %d, %s, %s", n, s, "0aU")
@@ -20,11 +21,12 @@ func TestEncoder(t *testing.T) {
 
 func BenchmarkEncoder(b *testing.B) {
 	b.StartTimer()
-	counter := NewCounter(int64(100000))
-	counter.Run()
+	//	counter := NewCounter(int64(100000))
+	//	counter.Run()
+	n := int64(100000)
 	for i := 0; i < b.N; i++ {
-		n := <-counter.ch
 		s := generateURL(n)
+		n++
 		decodeURL(s)
 	}
 }
